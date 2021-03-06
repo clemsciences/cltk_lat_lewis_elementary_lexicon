bcef7bf366e8fd583fb3ba170937564696081ee4
Clément Besnier <clemsciences@aol.com>
2021-03-06 22:46:13 +0100

    Added code to make json file

--- reader.py
+++ reader.py
@@ -1,11 +1,13 @@
 """
 
 """
-import codecs
+from collections import defaultdict
+import json
+import os
 
 from bs4 import BeautifulSoup
 from lxml import etree
-from yaml import dump
+import yaml
 
 
 def get_root(filename):
@@ -28,9 +30,48 @@ def get_entries(filename):
 
 def save_yaml(data, filename):
     with open(filename, "w") as f:
-        dump(data, f)
+        yaml.dump(data, f)
+
+
+def read_yaml(filename):
+    with open(filename, "r") as f:
+        return yaml.safe_load(f)
+
+
+def save_json(data, filename):
+    with open(filename, "w") as f:
+        json.dump(data, f)
+
+
+def read_json(filename):
+    with open(filename, "r") as f:
+        json.load(f)
+
+
+def save_json_by_letter(data, directory_name, ):
+    """
+    >>> entries = read_yaml("lewis.yaml")
+    >>> save_json_by_letter(entries, "entries")
+
+    :param data:
+    :param directory_name:
+    :return:
+    """
+    first_letters = set()
+    entries_by_letter = defaultdict(list)
+    for entry in data:
+        first_letters.add(entry[0])
+        entries_by_letter[entry[0].lower()].append({entry: data[entry]})
+    if not os.path.exists(directory_name):
+        os.mkdir(directory_name)
+    if os.path.exists(directory_name):
+        for i in entries_by_letter:
+            with open(os.path.join(directory_name, str(i) + ".json"), "w") as f:
+                json.dump(entries_by_letter[i], f)
 
 
 if __name__ == "__main__":
-    entries = get_entries("lewis.xml")
-    save_yaml(entries, "lewis.yaml")
+    # entries = get_entries("lewis.xml")
+    # save_yaml(entries, "lewis.yaml")
+    entries = read_yaml("lewis.yaml")
+    # save_json(data, "lewis.json")
